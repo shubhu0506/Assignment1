42b301123605a5a6965476cde7f6a27063360041
shubhu0506 <87603623+shubhu0506@users.noreply.github.com>
2022-10-18 17:35:44 +0530

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,22 @@
+
+public class Question4 {
+
+	public static void main(String[] args)
+	{
+		int n=14;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((j==0 && i>=n/2 -1) || i==n-1 || (j==n-1 && i>=n/2 -1) || i-j>=n/2 -1 || i+j>=n+n/2-2  )
+				{
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
